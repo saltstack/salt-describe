116f6ed6ee9256c68088571045893e82be35b986
Gareth J. Greenaway <gareth@saltstack.com>
2022-07-15 12:47:21 -0700

    initial work to break out runner functions into seperate files.

--- src/saltext/salt_describe/runners/salt_describe.py
+++ src/saltext/salt_describe/runners/salt_describe.py
@@ -119,50 +119,6 @@ def _generate_pillars(minion, pillar, sls_name="default"):
     return True
 
 
-def pkg(tgt, tgt_type="glob", include_version=True, single_state=True):
-    """
-    Gather installed pkgs on minions and build a state file.
-
-    CLI Example:
-
-    .. code-block:: bash
-
-        salt-run describe.pkg minion-tgt
-
-    """
-
-    ret = __salt__["salt.execute"](
-        tgt,
-        "pkg.list_pkgs",
-        tgt_type=tgt_type,
-    )
-
-    for minion in list(ret.keys()):
-        _pkgs = ret[minion]
-        if single_state:
-            if include_version:
-                pkgs = [{name: version} for name, version in _pkgs.items()]
-            else:
-                pkgs = list(_pkgs.keys())
-
-            state_contents = {"installed_packages": {"pkg.installed": [{"pkgs": pkgs}]}}
-            state = yaml.dump(state_contents)
-        else:
-            state_contents = {}
-            for name, version in _pkgs.items():
-                state_name = f"install_{name}"
-                if include_version:
-                    state_contents[state_name] = {
-                        "pkg.installed": [{"name": name, "version": version}]
-                    }
-                else:
-                    state_contents[state_name] = {"pkg.installed": [{"name": name}]}
-            state = yaml.dump(state_contents)
-
-        _generate_sls(minion, state, "pkg")
-
-    return True
-
 
 def file(tgt, paths, tgt_type="glob"):
     """
