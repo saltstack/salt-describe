071d2d0645b8bec731563cae4a97a35087389646
Gareth J. Greenaway <gareth@saltstack.com>
2023-02-22 08:46:51 -0800

    Adding more debugging to the service tests.

--- tests/integration/runners/test_salt_describe_service.py
+++ tests/integration/runners/test_salt_describe_service.py
@@ -1,7 +1,11 @@
 # Copyright 2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 #
+import logging
+
 import yaml
+
+log = logging.getLogger(__name__)
 
 
 def test_service(salt_run_cli, minion):
@@ -9,9 +13,10 @@ def test_service(salt_run_cli, minion):
     Test describe.service
     """
     ret = salt_run_cli.run("describe.service", tgt=minion.id)
-    if ret:
-        gen_sls = ret.data["Generated SLS file locations"][0]
-        with open(gen_sls) as fp:
-            data = yaml.safe_load(fp)
-        assert "service.running" in data[list(data.keys())[0]]
-        assert ret.returncode == 0
+    log.info("=== ret %s ===", ret)
+    assert ret == ""
+    gen_sls = ret.data["Generated SLS file locations"][0]
+    with open(gen_sls) as fp:
+        data = yaml.safe_load(fp)
+    assert "service.running" in data[list(data.keys())[0]]
+    assert ret.returncode == 0
