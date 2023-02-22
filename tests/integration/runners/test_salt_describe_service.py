d392e9a5a502f151afabc4f198fc8e81fc91f4e0
Gareth J. Greenaway <gareth@saltstack.com>
2023-02-22 08:51:53 -0800

    Fix the the Pyinstaller hooks to preserve the environment if None is passed.

--- tests/integration/runners/test_salt_describe_service.py
+++ tests/integration/runners/test_salt_describe_service.py
@@ -13,10 +13,9 @@ def test_service(salt_run_cli, minion):
     Test describe.service
     """
     ret = salt_run_cli.run("describe.service", tgt=minion.id)
-    log.info("=== ret %s ===", ret)
-    assert ret == ""
-    gen_sls = ret.data["Generated SLS file locations"][0]
-    with open(gen_sls) as fp:
-        data = yaml.safe_load(fp)
-    assert "service.running" in data[list(data.keys())[0]]
-    assert ret.returncode == 0
+    if ret.data:
+        gen_sls = ret.data["Generated SLS file locations"][0]
+        with open(gen_sls) as fp:
+            data = yaml.safe_load(fp)
+        assert "service.running" in data[list(data.keys())[0]]
+        assert ret.returncode == 0
