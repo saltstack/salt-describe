5436bc218449ff18678db5e060b9fb3fcd072e4d
Gareth J. Greenaway <gareth@saltstack.com>
2023-02-21 20:02:49 -0800

    moving the check further down.

--- tests/integration/runners/test_salt_describe_iptables.py
+++ tests/integration/runners/test_salt_describe_iptables.py
@@ -20,9 +20,9 @@ def test_iptables(salt_run_cli, minion):
     )
 
     ret = salt_run_cli.run("describe.iptables", tgt=minion.id)
-    assert ret == ""
     gen_sls = ret.data["Generated SLS file locations"][0]
     with open(gen_sls) as fp:
         data = yaml.safe_load(fp)
+    assert data == ""
     assert "chain" in data["add_iptables_rule_0"]["iptables.append"][0]
     assert ret.returncode == 0
