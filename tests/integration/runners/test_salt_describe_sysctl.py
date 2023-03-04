a45f1cc85240c38f58b07212803b7e9fdb9837cd
Gareth J. Greenaway <gareth@saltstack.com>
2023-03-04 10:28:27 -0800

    Account for different sysctl items on OSX.

--- tests/integration/runners/test_salt_describe_sysctl.py
+++ tests/integration/runners/test_salt_describe_sysctl.py
@@ -1,6 +1,8 @@
 # Copyright 2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 #
+import sys
+
 import pytest
 import yaml
 
@@ -10,9 +12,13 @@ def test_sysctl(salt_run_cli, minion):
     """
     Test describe.sysctl
     """
-    ret = salt_run_cli.run("describe.sysctl", tgt=minion.id, sysctl_items=["vm.swappiness"])
+    if sys.platform.startswith("darwin"):
+        sysctl_key = "vm.swapusage"
+    else:
+        sysctl_key = "vm.swappiness"
+    ret = salt_run_cli.run("describe.sysctl", tgt=minion.id, sysctl_items=[sysctl_key])
     gen_sls = ret.data["Generated SLS file locations"][0]
     with open(gen_sls) as fp:
         data = yaml.safe_load(fp)
-    assert data["sysctl-vm.swappiness"]["sysctl.present"][0]["name"] == "vm.swappiness"
+    assert data[f"sysctl-{sysctl_key}"]["sysctl.present"][0]["name"] == sysctl_key
     assert ret.returncode == 0
