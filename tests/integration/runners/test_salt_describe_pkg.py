991c23f9804c2612893ac92d4ee4b7aa8df30126
Gareth J. Greenaway <gareth@saltstack.com>
2023-03-06 11:52:37 -0800

    skip pkg tests on OS X is brew is not available.

--- tests/integration/runners/test_salt_describe_pkg.py
+++ tests/integration/runners/test_salt_describe_pkg.py
@@ -1,9 +1,11 @@
 # Copyright 2023 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 #
+import pytest
 import yaml
 
 
+@pytest.mark.skip_if_binaries_missing("brew")
 def test_pkg(salt_run_cli, minion):
     """
     Test describe.pkg
